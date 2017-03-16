74854906ac8691c0ae0ca2fd6fada3ea350a9bc4
Uzochukwu Odozi <uzo.odozi@gmail.com>
2017-03-16 16:37:19 +0200

    Changed the method used to make the api request inside the server.js file

--- server.js
+++ server.js
@@ -1,38 +1,59 @@
-//Load the required modules
+//Load the required modules 
+//The http module comes with node. It doesn;t have to be installed
 var http = require('http')
-var server = http.createServer();
 
-var request = require('request');
+//This module allows us to parse url object
 var url = require('url');
 
-server.on('request', function(req, res){
+//Use whatever port is in the environment variable or port 3000
+var port = process.env.PORT || 3000
+
+//The createSever method creates a web server object
+//The anonymous function or callback function is called a request handler
+var httpServer = http.createServer(function(req, res){
+    //This adds a path to the url
     var query = url.parse(req.url, true).path;
-    request.get({
-        url: 'http://api.fixer.io'+query,
+    
+    http.get({
+        host: 'api.fixer.io',
+        path: query,
         headers: {
-            'User-Agent': 'request',
             'Content-Type': 'application/json'
         }
-    }, function(err, response, body){
-        if(err){
-            res.write('Unable to connect to server');
-            res.end();
-        }else if(response.statusCode === 400){
-            res.write('Unable to find address');
-            res.end();
-        }else if(response.statusCode === 200){
-            res.write(response.body);
-            res.end();  
-        }
+    }, function(response) {
+        var body = '';
         
+        //The data and end events are used to grab the data that is returned
+        //The data returned is a buffer
+        response.on('data', function(d) {
+            body += d;
+        });
+        
+        //This event handles errors
+        response.on('error', function(err){
+            if(err){
+                res.write('Unable to connect to server');
+                res.end();
+            }else if(response.statusCode === 400){
+                res.write('Unable to find address');
+                res.end();
+            }
+        })
+        
+        response.on('end', function() {
+            if(response.statusCode === 200){
+                res.write(body); 
+                res.end()
+            }           
+        });
     });
 });
 
 
-var port = process.env.port || 8080
-server.listen(port, function(){
-    console.log('Listening on port '+port);
+//The listen method is called on the server object in other to server requests
+httpServer.listen(port, function(){
+    console.log('Listening on port ' + port);
 });
 
-module.exports = server;
+module.exports = httpServer;
 
